0572615b3eb24f32cebb362f7b9575cc6e147eba
Giorgi <vasokima@gmail.com>
2025-06-19 22:49:49 +0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const CACHE_NAME = 'quoteverse-v1';
-const urlsToCache = ['/', '/index.html'];
+const urlsToCache = ['/', '/index.html', '/icon-192x192.png', '/icon-512x512.png'];
 
 self.addEventListener('install', event => {
     event.waitUntil(
@@ -12,5 +12,20 @@ self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
             .then(response => response || fetch(event.request))
+    );
+});
+
+self.addEventListener('activate', event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cacheName => {
+                    if (!cacheWhitelist.includes(cacheName)) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
     );
 });
